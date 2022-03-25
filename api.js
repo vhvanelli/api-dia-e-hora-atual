7ba189a5c1403295da22c51ee627da8b62ca164a
Victor Hugo Faria Vanelli <vhvanelli@gmail.com>
2022-03-25 15:46:35 -0300

    Added validade opening hour

--- api.js
+++ api.js
@@ -1,21 +1,30 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 8877;
+const PORT = process.env.PORT || 8877;
 
 const now = new Date;
+let horarioDeAtendimento;
 
 //DIA DA SEMANA
 const daysOfWeek = ["Domingo","Segunda","Terça","Quarta","Quinta","Sexta","Sabado"]
 const day = daysOfWeek[now.getDay()];
 
 //HORA DO DIA
-const hour =  now.getHours();
+const hour =  now.getHours() - 3;
+
+//VALIDAÇÃO DE DE HORARIO DE ATENDIMENTO
+if(day != "Domingo" && hour >= 8 && hour <= 18){
+    horarioDeAtendimento = true;
+} else {
+    horarioDeAtendimento = false;
+}
+
 
 app.get('/', (req, res) => {
-    res.send("Funcionando")
+    res.send("Funcionando");
 })
 app.post('/dia-hora', (req, res) => {
-    res.status(202).json({ dia: day, hora: hour })
+    res.status(202).json({ dia: day, hora: hour, horarioAtendimento : horarioDeAtendimento });
 })
 
- app.listen(port, () => console.log('Rodando na no localhost:3000'));
+ app.listen(PORT);
