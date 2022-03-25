e86bc286c87203a95650a401ed5980979a26868e
Victor Hugo Faria Vanelli <vhvanelli@gmail.com>
2022-03-25 16:36:19 -0300

    Fixed open hours logic

--- api.js
+++ api.js
@@ -3,28 +3,27 @@ const app = express();
 const PORT = process.env.PORT || 8877;
 
 const now = new Date;
-let horarioDeAtendimento;
+let openHours;
 
 //DIA DA SEMANA
 const daysOfWeek = ["Domingo","Segunda","Terça","Quarta","Quinta","Sexta","Sabado"]
+
 const day = daysOfWeek[now.getDay()];
 
 //HORA DO DIA
 const hour =  now.getHours() - 3;
 
 //VALIDAÇÃO DE DE HORARIO DE ATENDIMENTO
-if(day != "Domingo" && hour >= 8 && hour <= 18){
-    horarioDeAtendimento = true;
-} else {
-    horarioDeAtendimento = false;
-}
+openHours = !((day == "Domingo" || day == "Sexta" || day == "Sabado") || hour < 8 || hour > 18);
 
-
+console.log(openHours);
+console.log(hour);
+console.log(day);
 app.get('/', (req, res) => {
     res.send("Funcionando");
 })
 app.post('/dia-hora', (req, res) => {
-    res.status(202).json({ dia: day, hora: hour, horarioAtendimento : horarioDeAtendimento });
+    res.status(202).json({ dia: day, hora: hour, horarioAtendimento : openHours });
 })
 
  app.listen(PORT);
